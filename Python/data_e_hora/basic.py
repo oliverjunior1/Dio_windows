99ca4476cfaa6fb0deaf45646c61e792b8a725f0
oliverjunior1 <oliver-junior@hotmail.com>
2025-05-14 22:01:16 -0300

    Update basic.py

--- Python/data_e_hora/basic.py
+++ Python/data_e_hora/basic.py
@@ -1,6 +1,14 @@
-import datetime
+from datetime import date, time, datetime
 
-x = datetime.date(2025,5,14)
-y = datetime.time(21,20,50)
+x = date(2025,5,14)
+y = time(21,20,50)
 print(x)
 print(y)
+print(date.today())
+
+data_hora = datetime(2023,7,10,10,30,20)
+print(data_hora)
+print(datetime.today())
+
+hora = time(10,20,0)
+print(hora)
