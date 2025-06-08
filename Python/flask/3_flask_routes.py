7fed53c2a358d3ee62046ee32c79b4e9bfc3bbe0
oliverjunior1 <oliver-junior@hotmail.com>
2025-06-07 23:19:48 -0300

    Update 3_flask_routes.py

--- Python/flask/3_flask_routes.py
+++ Python/flask/3_flask_routes.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 @app.route('/hello')
 
-# after the adress put /hello
+# after the adress put /hello or what is unused down
 def hello_world():
     return 'Hello World!'
+
+# app.add_url_rule('/', 'hello', hello_world)
